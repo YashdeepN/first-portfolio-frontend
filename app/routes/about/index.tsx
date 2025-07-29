53e770d3c8ac13628cc6269cd292a1e2833fe2fa
YashdeepN <yashdeepneema@hotmail.com>
2025-07-29 01:03:57 -0500

    create all basic routes for the app

--- app/routes/about/index.tsx
+++ app/routes/about/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const AboutPage = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold text-white mb-2">
+    <section>
+      <h2 className="text-3xl font-bold text-white mb-2">
         Hey, I'm Yashdeep 👋
-      </h1>
-    </>
+      </h2>
+    </section>
   );
 };
 
--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -2,11 +2,11 @@ import type { Route } from "./+types/index";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "The Friendly Dev" },
+    { title: "The Friendly Dev | Welcome" },
     { name: "description", content: "A web devaloper portfolio" },
   ];
 }
 
 export default function Home() {
-  return <>My Portfolio</>;
+  return <section>My Portfolio</section>;
 }
