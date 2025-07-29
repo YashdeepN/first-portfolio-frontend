0ac6c2f386a3b9b053429bf6cc05fcd53f00eca4
YashdeepN <yashdeepneema@hotmail.com>
2025-07-29 08:57:06 -0500

    active link styling

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,6 +2,8 @@ import { FaLaptopCode } from "react-icons/fa";
 import { NavLink } from "react-router";
 
 const NavBar = () => {
+  const baseClass = "transition hover:text-blue-400";
+  const activeClass = "text-blue-400 font-semi-bold";
   return (
     <nav className="bg-gray-800 border-b border-gray-700 shadow-md sticky top-0 z-50">
       <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
@@ -16,11 +18,36 @@ const NavBar = () => {
         {/* Desktop Nav */}
         <div className="hidden md:flex items-center gap-6">
           <div className="space-x-4 text-sm text-gray-300">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/projects">Projects</NavLink>
-            <NavLink to="/blog">Blog</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? activeClass : baseClass)}
+              to="/"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? activeClass : baseClass)}
+              to="/projects"
+            >
+              Projects
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? activeClass : baseClass)}
+              to="/blog"
+            >
+              Blog
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? activeClass : baseClass)}
+              to="/about"
+            >
+              About
+            </NavLink>
+            <NavLink
+              className={({ isActive }) => (isActive ? activeClass : baseClass)}
+              to="/contact"
+            >
+              Contact
+            </NavLink>
           </div>
         </div>
       </div>
