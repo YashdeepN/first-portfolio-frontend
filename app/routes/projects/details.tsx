63c884e3d025b928b41bd42b9859b721d22d665b
YashdeepN <yashdeepneema@hotmail.com>
2025-07-30 01:06:07 -0500

    create details page markup

--- app/routes/projects/details.tsx
+++ app/routes/projects/details.tsx
@@ -1,5 +1,7 @@
 import type { Project } from "~/types";
 import type { Route } from "./+types/details";
+import { Link } from "react-router";
+import { FaArrowLeft } from "react-icons/fa6";
 
 export async function clientLoader({
   request,
@@ -19,9 +21,44 @@ export function HydrateFallback() {
 
 const ProjectDetailsPage = ({ loaderData }: Route.ComponentProps) => {
   const project = loaderData;
-  console.log(project);
 
-  return <div>ProjectDetailsPage</div>;
+  return (
+    <>
+      <Link
+        to="/projects"
+        className="flex items-center text-blue-400 hover:text-blue-500"
+      >
+        <FaArrowLeft className="mr-2" /> Back to Projects
+      </Link>
+
+      <div className="grid gap-8 md:grid-cols-2 items-start">
+        <div>
+          <img
+            src={project.image}
+            alt={project.title}
+            className="w-full rounded-lg shadow-md"
+          />
+        </div>
+        <div>
+          <h1 className="text-3xl font-bold text-blue-400 mb-4">
+            {project.title}
+          </h1>
+
+          <p className="text-gray-300 text-sm mb-4">
+            {new Date(project.date).toLocaleDateString()} • {project.category}
+          </p>
+          <p className="text-gray-200 mb-6">{project.description}</p>
+          <a
+            href={project.url}
+            target="_blank"
+            className="inline-block text-white bg-blue-600 hover:bg-blue-700 px-6 py-2 rounded transition"
+          >
+            View Live Site {"\u2794"}
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProjectDetailsPage;
