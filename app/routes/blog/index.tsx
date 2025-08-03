b5a77bfe9e342eb8551d3ad97ef7a56936e26b6b
YashdeepN <yashdeepneema@hotmail.com>
2025-08-02 23:34:51 -0500

    fetch and show blog metadata

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,9 +1,47 @@
 import React from "react";
+import type { Route } from "./+types/index";
+import type { PostMeta } from "~/types";
+import { Link } from "react-router";
+import { FaArrowRight } from "react-icons/fa";
 
-const BlogPage = () => {
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+  const url = new URL("/posts-meta.json", request.url);
+  const res = await fetch(url.href);
+
+  if (!res.ok) throw new Error("Failed to fetch data");
+
+  const data = await res.json();
+
+  return { posts: data };
+}
+
+const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const { posts } = loaderData;
+
   return (
     <>
-      <h2 className="text-3xl text-white font-bold mb-8">📝 Blog</h2>
+      <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
+        <h2 className="text-3xl text-white font-bold mb-8">📝 Blog</h2>
+        {posts.map((post) => (
+          <article key={post.slug} className="bg-gray-800 p-6 rounded-lg mb-4">
+            <h3 className="text-2xl font-semibold text-blue-400">
+              {post.title}
+            </h3>
+            <p className="text-sm text-gray-400 mb-2">
+              {new Date(post.date).toLocaleDateString()}
+            </p>
+            <p className="text-gray-300 mb-4">{post.excerpt}</p>
+            <Link
+              to={`/blog/${post.slug}`}
+              className="text-blue-300 text-sm hover:underline"
+            >
+              Read More ➔
+            </Link>
+          </article>
+        ))}
+      </div>
     </>
   );
 };
