dff55caeb86ce50af518fcc9a7a84dcd39014c77
YashdeepN <yashdeepneema@hotmail.com>
2025-08-03 07:25:35 -0500

    implement pagination to blog posts

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-import type { Route } from "./+types/index";
-import type { PostMeta } from "~/types";
-import { Link } from "react-router";
-import { FaArrowRight } from "react-icons/fa";
 import PostCard from "~/components/PostCard";
+import type { PostMeta } from "~/types";
+import type { Route } from "./+types/index";
+import { useState } from "react";
+import Pagination from "~/components/Pagination";
 
 export async function loader({
   request,
@@ -19,15 +18,31 @@ export async function loader({
 }
 
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const postPerPage = 3;
+
   const { posts } = loaderData;
+
+  const totalPages = Math.ceil(posts.length / postPerPage);
+  const indexOfLast = currentPage * postPerPage;
+  const indexOfFirst = indexOfLast - postPerPage;
+
+  const currentPosts = posts.slice(indexOfFirst, indexOfLast);
 
   return (
     <>
       <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
         <h2 className="text-3xl text-white font-bold mb-8">📝 Blog</h2>
-        {posts.map((post) => (
+        {currentPosts.map((post) => (
           <PostCard key={post.slug} post={post} />
         ))}
+        {totalPages > 1 && (
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={(page) => setCurrentPage(page)}
+          />
+        )}
       </div>
     </>
   );
