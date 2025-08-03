0d4bef4151cd9616513a312d92e4c18f194375bb
YashdeepN <yashdeepneema@hotmail.com>
2025-08-03 08:01:02 -0500

    implement search filter for posts

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -3,6 +3,7 @@ import type { PostMeta } from "~/types";
 import type { Route } from "./+types/index";
 import { useState } from "react";
 import Pagination from "~/components/Pagination";
+import PostFilter from "~/components/PostFilter";
 
 export async function loader({
   request,
@@ -18,24 +19,47 @@ export async function loader({
 }
 
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const [searchQuery, setSearchQuery] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
   const postPerPage = 3;
 
   const { posts } = loaderData;
 
-  const totalPages = Math.ceil(posts.length / postPerPage);
+  const filteredPosts = posts.filter((post) => {
+    const query = searchQuery.toLocaleLowerCase();
+    return (
+      post.title.toLowerCase().includes(query) ||
+      post.excerpt.toLowerCase().includes(query)
+    );
+  });
+
+  const totalPages = Math.ceil(filteredPosts.length / postPerPage);
   const indexOfLast = currentPage * postPerPage;
   const indexOfFirst = indexOfLast - postPerPage;
 
-  const currentPosts = posts.slice(indexOfFirst, indexOfLast);
+  const currentPosts = filteredPosts.slice(indexOfFirst, indexOfLast);
 
   return (
     <>
       <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
         <h2 className="text-3xl text-white font-bold mb-8">📝 Blog</h2>
-        {currentPosts.map((post) => (
-          <PostCard key={post.slug} post={post} />
-        ))}
+
+        <PostFilter
+          searchQuery={searchQuery}
+          onSearchChange={(query) => {
+            setSearchQuery(query);
+            setCurrentPage(1);
+          }}
+        />
+
+        <div className="space-y-8">
+          {currentPosts.length === 0 ? (
+            <p className="text-gray-400 text-center">No posts found</p>
+          ) : (
+            currentPosts.map((post) => <PostCard key={post.slug} post={post} />)
+          )}
+        </div>
+
         {totalPages > 1 && (
           <Pagination
             currentPage={currentPage}
