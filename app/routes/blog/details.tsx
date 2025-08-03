fafc3fd8088cad247ef43515cd42b9e42a5a2443
YashdeepN <yashdeepneema@hotmail.com>
2025-08-03 07:16:45 -0500

    display blog posts and create new post

--- app/routes/blog/details.tsx
+++ app/routes/blog/details.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import type { Route } from "./+types/details";
 import type { PostMeta } from "~/types";
+import { Link } from "react-router";
+import { FaArrowLeft } from "react-icons/fa";
 
 export async function loader({ request, params }: Route.LoaderArgs) {
   const { slug } = params;
@@ -24,9 +26,32 @@ export async function loader({ request, params }: Route.LoaderArgs) {
 
 const BlogDetailsPage = ({ loaderData }: Route.ComponentProps) => {
   const { postMeta, markdown } = loaderData;
-  console.log(postMeta, markdown);
 
-  return <div>So</div>;
+  return (
+    <div className="max-w-3xl mx-auto px-6 py-12 bg-gray-900">
+      <h1 className="text-3xl font-bold text-blue-400 mb-2">
+        {postMeta.title}
+      </h1>
+      <div className="text-sm text-gray-400 mb-6">
+        {new Date(postMeta.date).toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}
+      </div>
+
+      <div className="prose prose-invert max-w-none mb-12">
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </div>
+
+      <Link
+        to="/blog"
+        className="flex items-center text-blue-400 hover:text-blue-500"
+      >
+        <FaArrowLeft className="mr-2" /> Back To Posts
+      </Link>
+    </div>
+  );
 };
 
 export default BlogDetailsPage;
