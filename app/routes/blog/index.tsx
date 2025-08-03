4c134b259b0131e1dd5752cbd8fda516e157e0d6
YashdeepN <yashdeepneema@hotmail.com>
2025-08-02 23:38:53 -0500

    extract out the PostCard component

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -3,6 +3,7 @@ import type { Route } from "./+types/index";
 import type { PostMeta } from "~/types";
 import { Link } from "react-router";
 import { FaArrowRight } from "react-icons/fa";
+import PostCard from "~/components/PostCard";
 
 export async function loader({
   request,
@@ -25,21 +26,7 @@ const BlogPage = ({ loaderData }: Route.ComponentProps) => {
       <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
         <h2 className="text-3xl text-white font-bold mb-8">📝 Blog</h2>
         {posts.map((post) => (
-          <article key={post.slug} className="bg-gray-800 p-6 rounded-lg mb-4">
-            <h3 className="text-2xl font-semibold text-blue-400">
-              {post.title}
-            </h3>
-            <p className="text-sm text-gray-400 mb-2">
-              {new Date(post.date).toLocaleDateString()}
-            </p>
-            <p className="text-gray-300 mb-4">{post.excerpt}</p>
-            <Link
-              to={`/blog/${post.slug}`}
-              className="text-blue-300 text-sm hover:underline"
-            >
-              Read More ➔
-            </Link>
-          </article>
+          <PostCard key={post.slug} post={post} />
         ))}
       </div>
     </>
